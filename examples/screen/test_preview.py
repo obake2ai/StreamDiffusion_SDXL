91388c8db4e064184a95f0d4c4dd856e0ffda761
Yuma Kishi <obake2ai@gmail.com>
2024-09-17 18:21:24 +0900

    add test code

--- examples/screen/test_preview.py
+++ examples/screen/test_preview.py
@@ -7,6 +7,8 @@ from multiprocessing import Process, Queue, get_context
 from streamdiffusion.image_utils import pil2tensor
 from utils.wrapper import StreamDiffusionWrapper
 
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
 # 動画のプレビューおよび画像生成の組み込み
 def image_generation_process(queue, model_id_or_path, prompt, negative_prompt, width, height):
     # StreamDiffusionWrapper の設定
