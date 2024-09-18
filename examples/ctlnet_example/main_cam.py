887d7613db22ceeed43bbb3360f9e88d1d8a3831
Yuma Kishi <obake2ai@gmail.com>
2024-09-18 18:48:22 +0900

    bug fix

--- examples/ctlnet_example/main_cam.py
+++ examples/ctlnet_example/main_cam.py
@@ -672,7 +672,7 @@ def image_generation_process(
     adapter = True
     ip_adapter_image_filepath = "assets/xshingoboy-0043.jpg"
 
-    t_index_list = [15, 25]
+    t_index_list = [0, 25]
     cfg_type = "none"
     delta = 1.0
 
