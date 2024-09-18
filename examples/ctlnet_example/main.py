55027ce1396ed59ee17f693b220338a70520df3c
Yuma Kishi <obake2ai@gmail.com>
2024-09-18 15:46:39 +0900

    updates

--- examples/ctlnet_example/main.py
+++ examples/ctlnet_example/main.py
@@ -801,7 +801,7 @@ def image_generation_process(
 
 def main(
     model_id_or_path: str = "Lykon/dreamshaper-8-lcm",
-    lora_dict: Optional[Dict[str, float]] = {"./models/LoRA/xshingoboy.safetensors":1.0},,
+    lora_dict: Optional[Dict[str, float]] = {"./models/LoRA/xshingoboy.safetensors":1.0},
     prompt: str = "xshingoboy",
     negative_prompt: str = "low quality, bad quality, blurry, low resolution",
     frame_buffer_size: int = 1,
