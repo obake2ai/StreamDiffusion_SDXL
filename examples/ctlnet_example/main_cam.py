b5510ff00edf5c4e7edeeffb0e14f1062f8b9230
Yuma Kishi <obake2ai@gmail.com>
2024-09-18 18:26:33 +0900

    add sub monitor

--- examples/ctlnet_example/main_cam.py
+++ examples/ctlnet_example/main_cam.py
@@ -675,7 +675,7 @@ def image_generation_process(
     adapter = True
     ip_adapter_image_filepath = "assets/xshingoboy-0043.jpg"
 
-    t_index_list = [18, 37]
+    t_index_list = [0, 25, 45]
     cfg_type = "none"
     delta = 1.0
 
