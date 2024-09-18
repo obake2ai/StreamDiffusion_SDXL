ac77ccdc726e3bf0f94f0ee4f0aeb8ba77b2296d
Yuma Kishi <obake2ai@gmail.com>
2024-09-18 18:40:40 +0900

    bug fix

--- examples/ctlnet_example/main_cam.py
+++ examples/ctlnet_example/main_cam.py
@@ -665,14 +665,14 @@ def image_generation_process(
 
     global inputs
     global box_prompt
-    instep = 50
+    instep = 30
     ######################################################
     # パラメタ
     ######################################################
     adapter = True
     ip_adapter_image_filepath = "assets/xshingoboy-0043.jpg"
 
-    t_index_list = [0, 25, 45]
+    t_index_list = [0, 25]
     cfg_type = "none"
     delta = 1.0
 
