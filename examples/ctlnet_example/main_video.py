8dfda1683b8b6826ecdc9df57124045a33bfc869
Yuma Kishi <obake2ai@gmail.com>
2024-09-19 23:48:25 +0900

    bug fix

--- examples/ctlnet_example/main_video.py
+++ examples/ctlnet_example/main_video.py
@@ -48,7 +48,7 @@ class StreamDiffusionControlNetSample(StreamDiffusion):
                  use_denoising_batch: bool = True,
                  frame_buffer_size: int = 1,
                  cfg_type: Literal["none", "full", "self", "initialize"] = "self",
-                 acceleration: Literal["none", "xformers", "tensorrt"],
+                 acceleration: Literal["none", "xformers", "tensorrt"] = "tensorrt",
                  engine_dir: Optional[Union[str, Path]] = "engines",
                  ip_adapter=None):
         super().__init__(pipe,
